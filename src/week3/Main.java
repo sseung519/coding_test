2483d2da4e4281686eea1ed8b105d06f053ad4eb
sseung519 <htc_desirehd@naver.com>
2024-08-23 10:27:50 +0900

    .

--- src/week3/Main.java
+++ src/week3/Main.java
@@ -1,4 +1,4 @@
-package week4;
+package week3;
 
 import java.io.*;
 import java.util.Arrays;
--- src/week3/test.java
+++ src/week3/test.java
@@ -1,4 +1,4 @@
-package week4;
+package week3;
 
 public class test {
     public static void main(String[] args) {
